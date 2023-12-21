8888379f33815135f2d40a84458c3c716ddaac3e
Sidhant Acharya <acharyasid1@gmail.com>
2023-12-21 22:01:49 +0545

    post and patch api

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const app=require("express")()
+const bodyParser= require("body-parser")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true,
+}))
 
 app.listen(3000,()=>console.log("connected to server"))
 
